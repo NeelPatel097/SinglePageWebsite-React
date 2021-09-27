e7f6ce4a9b1663c9c520a2f4704070a2a1216bc8
Neel Patel <np7.patel.np@gmail.com>
2021-09-27 23:29:36 +0530

    Sidebar 2

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {SidebarContainer, Icon, CloseIcon} from './SidebarElements';
 
 const Sidebar = () => {
     return (
@@ -6,6 +7,28 @@ const Sidebar = () => {
             <Icon>
                 <CloseIcon />
             </Icon>
+            <SidebarWrapper>
+                <SidebarMenu>
+                    <SidebarLink to="about">
+                        Home
+                    </SidebarLink>
+                    <SidebarLink to="about">
+                        About
+                    </SidebarLink>
+                    <SidebarLink to="solutions">
+                        Solutions
+                    </SidebarLink>
+                    <SidebarLink to="founder">
+                        Founder
+                    </SidebarLink>
+                    <SidebarLink to="career">
+                        Career
+                    </SidebarLink>
+                    <SidebarLink to="career">
+                        Get in touch
+                    </SidebarLink>
+                </SidebarMenu>
+            </SidebarWrapper>
 
         </SidebarContainer>
     );
